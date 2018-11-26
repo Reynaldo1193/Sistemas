a61761d3ea4ace9cf1acceffa5d1fe253b830456
Reynaldo <reynaldo119@gmail.com>
2018-11-26 13:52:41 -0600

    fitness function

--- GeneticAlgorihtms/DNA.py
+++ GeneticAlgorihtms/DNA.py
@@ -16,3 +16,15 @@ class DNA:
     def getPhrase(self):
         phrase = "".join(self.genes)
         return phrase
+
+    def calcFitness(self,target):
+        self.score = 0
+        self.fitness = 0.0
+        self.targetSize = len(target)
+        for i in range(0,self.targetSize):
+            if(self.genes[i] == target[i]):
+                self.score = self.score+1
+        self.fitness =  float(self.score)/float(self.targetSize)
+        print(self.score)
+        print(self.targetSize)
+        print(self.fitness)
--- GeneticAlgorihtms/Population.py
+++ GeneticAlgorihtms/Population.py
@@ -2,15 +2,20 @@
 
 import DNA
 
+populationSize = 10
 target = "To be or not to be"
 
 class Population():
     """docstring forPopulation."""
     def __init__(self,target):
         self.population = []
-        populationSize = 50
         for i in range(0,populationSize):
             self.population.append(DNA.DNA(len(target)))
 
+    def calcFitness(self):
+        for i in range(0,populationSize):
+            self.population[i].calcFitness(target)
+            print(self.population[i].getPhrase())
+
 poblacion = Population(target)
-print(poblacion.population[10].genes)
+poblacion.calcFitness()
